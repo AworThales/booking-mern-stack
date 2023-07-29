60628e44ebd00946cec00574bb21b92ebb75d0e2
AworThales <thals4jesus@gmail.com>
2023-07-29 15:06:07 +0100

    Initializing directory

---
+++ frontend/src/components/navbar/NavBar.jsx
@@ -0,0 +1,9 @@
+import "./navBar.css";
+
+export default function NavBar() {
+    return (
+     <div className="navBar">
+      Navabr
+     </div>
+    );
+  }
---
+++ frontend/src/pages/hotel/Hotel.jsx
@@ -0,0 +1,9 @@
+import "./hotel.css";
+
+export default function Hotel() {
+    return (
+     <div className="hotel">
+        Hotel
+     </div>
+    );
+  }
