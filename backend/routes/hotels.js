6b3b3f2d979a62081e719fc1b7e7393253b57baa
AworThales <thals4jesus@gmail.com>
2023-08-08 12:11:48 +0100

    Moving Functions to it controller

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,73 +1,26 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+import { 
+    createHotel, deleteHotel, 
+    getAllHotel, getHotel,
+    updateHotel,
+} from "../controllers/hotelController.js";
+
+
 const router = express.Router();
 
-// Create
-router.post("/", async (req,res) => {
+// Create hotel route
+router.post("/", createHotel);
 
-    // Recieving data from the user
-    const newHotel = new Hotel(req.body);
-    try{
-        const saveHotel = await newHotel.save();
-        res.status(200).json(saveHotel);
-    }
-    catch(err){
-        res.status(500).json(err); //server error
-    }
-});
+// Update Hotel route
+router.put("/:id", updateHotel);
 
-// Update
-router.put("/:id", async (req, res) =>{
-    try {
-        const updateHotel = await Hotel.findByIdAndUpdate(
-            req.params.id, // geting requesting query params
-            { $set: req.body},  // we update using mongodb $set mtd, what to is inside body
-            {new: true} )  // setting new true will update our new data on mongodb else it will not update
-        res.status(200).json(updateHotel);
-    } catch (err) {
-        res.status(500).json(err)
-    }
-});
+// Delete hotel route
+router.delete("/:id", deleteHotel);
 
-// Delete
-router.delete("/:id", async (req, res) => {
-    try{
-        // we won't create a vairable since we are not returning anything
-        await Hotel.findByIdAndDelete(req.params.id);
-        res.status(200).json("Hotel deleted successfully")
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-});
+// Get an hotel route
+router.get("/:id", getHotel);
 
-// Get
-router.get("/:id", async (req, res) =>{
-    try{
-        const oneHotel = await Hotel.findById(
-            req.params.id
-        );
-        res.status(200).json(oneHotel);
-    }
-    catch(err){
-        res.status(500).json(err);
-    }
-})
-
-// Get All
-// Get
-router.get("/", async (req, res, next) =>{
-    const failed = true;
-    if(failed) return next(createError(404, "You are not authenticated"));
-
-    try{
-        const allHotels = await Hotel.findById("jdddhfhfhf");
-        res.status(200).json(allHotels);
-    }
-    catch(err){
-        next(err);
-    }
-})
+// Get All hotel route
+router.get("/", getAllHotel);
 
 export default router
