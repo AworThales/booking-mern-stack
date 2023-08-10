45829cfbef2264001543f078c3eff3fcef9a21fd
AworThales <thals4jesus@gmail.com>
2023-08-10 15:33:26 +0100

    update

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+},
+    {timestamps: true} //Timestamps true is going to give us created_At and updated_At
+);
+
+export default mongoose.model("User", UserSchema);
