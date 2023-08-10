cfe8858a9b2acef6f307f63947602e9413d12880
AworThales <thals4jesus@gmail.com>
2023-08-10 15:26:18 +0100

    Room route updated

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,5 +1,28 @@
 import express from 'express';
+import { verifyAdmin } from '../utils/verification.js';
+import { 
+    createRoom, 
+    deletRoom, 
+    getAllRoom, 
+    getRoom, 
+    updateRoom
+} from '../controllers/roomController.js';
+
 const router = express.Router();
 
+/** Create Room Route */
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+/** Update Room Route */
+router.put("/:id", verifyAdmin, updateRoom);
+
+/** Delete Room route */
+router.delete("/:id/:hotelId", verifyAdmin, deletRoom);
+
+/** Get Room Route */
+router.get("/:id", getRoom);
+
+/** Get All Room Route */
+router.get("/", getAllRoom);
 
 export default router
