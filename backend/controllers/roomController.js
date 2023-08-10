98953cdf15910940f97afcd99305648c83966211
AworThales <thals4jesus@gmail.com>
2023-08-10 15:24:09 +0100

    Room controller updated

---
+++ backend/controllers/roomController.js
@@ -0,0 +1,88 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+
+/** Create Room Function */
+export const createRoom = async (req, res, next) =>{
+    const hotelId = req.params.hotelId;
+    const newRoom = new Room(req.body)
+    try{
+        const saveRoom = await newRoom.save();
+
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $push: {rooms: saveRoom._id}
+            });
+        } catch (err) {
+            next(err);
+        }
+
+        res.status(200).json(saveRoom);
+    }
+    catch(err){
+        next(err);
+    };
+};
+
+/** Update Room Fuction */
+export const updateRoom = async (req, res, next) =>{
+    try{
+        const updateRomm = await Room.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body},
+            {new: true}
+        );
+
+        res.status(200).json(updateRomm);
+    }
+    catch(err) {
+        next(err);
+    };
+};
+
+
+/** Delete Romm Function */
+export const deletRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelId;
+    try{
+        await Room.findByIdAndDelete(req.params.id );
+        
+        try {
+            await Hotel.findByIdAndUpdate(hotelId, {
+                $pull: {rooms: req.params.id}
+            });
+        } catch (err) {
+            next(err);
+        };
+
+        res.status(200).json("Room deleted successfully...!");
+    }
+    catch(err) {
+        next(err);
+    };
+};
+
+/** Get Room Function */
+export const getRoom = async (req, res, next) => {
+    try{
+        const oneRoom = await Room.findById(
+            req.params.id
+        );
+        res.status(200).json(oneRoom);
+    }
+    catch(err) {
+        next(err);
+    };
+};
+
+/** Get All Room function */
+export const getAllRoom = async (req, res, next) => {
+    try{
+        const allRooms =await Room.find(
+            req.params.id
+        );
+        res.status(200).json(allRooms);
+    }
+    catch(err) {
+        next(err);
+    };
+};
