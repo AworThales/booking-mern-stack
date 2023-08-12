1e5bdefb495eab9a83b704f2de4a9836081697f9
AworThales <thals4jesus@gmail.com>
2023-08-12 14:38:11 +0100

    Passing query to getAllHotel Api

--- backend/controllers/hotelController.js
+++ backend/controllers/hotelController.js
@@ -56,8 +56,12 @@ export const getHotel = async (req, res, next) => {
 
 /** Api function to get all hotel */
 export const getAllHotel = async (req, res, next) => {
+    const {min, max, ...others} = req.query;
     try{
-        const allHotels = await Hotel.find();
+        const allHotels = await Hotel.find({
+            ...others,
+            cheapestPrice: { $gt: min || 1, $lt: max || 999 },
+        }).limit(req.query.limit);
         res.status(200).json(allHotels);
     }
     catch(err){
