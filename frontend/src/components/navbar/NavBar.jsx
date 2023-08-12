2076b48571077bf4f52b02e8f1f47a1817d86951
AworThales <thals4jesus@gmail.com>
2023-08-12 14:08:07 +0100

    Implemented home route

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,10 +1,13 @@
 import "./navBar.css";
+import { Link } from "react-router-dom"
 
 export default function NavBar() {
     return (
      <div className="navBar">
         <div className="navBarContainer">
-          <span className="navLogo">TBooking</span>
+          <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
+            <span className="navLogo">TBooking</span>
+          </Link>
           <div className="navBarItems">
             <button className="navBarButton">Register</button>
             <button className="navBarButton">Login</button>
