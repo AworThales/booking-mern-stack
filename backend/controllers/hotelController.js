8c94a1adc26a15411adb9025f83d8f05affb19b8
AworThales <thals4jesus@gmail.com>
2023-08-12 11:40:37 +0100

    Added CountByCity and CountByType Api fn

--- backend/controllers/hotelController.js
+++ backend/controllers/hotelController.js
@@ -64,3 +64,38 @@ export const getAllHotel = async (req, res, next) => {
         next(err);
     }
 };
+
+/** Api function to Count by cities */
+export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(",");
+    try{
+        const list = await Promise.all(cities.map((city) => {
+            return Hotel.countDocuments({city: city});
+        }));
+        res.status(200).json(list);
+    }
+    catch(err){
+        next(err);
+    };
+};
+
+/** Api function to Count by type */
+export const countByType = async (req, res, next) => {
+    try{
+        const countHotel = await Hotel.countDocuments({type: "Hotel"});
+        const countApartment = await Hotel.countDocuments({type: "apartment"});
+        const countResort = await Hotel.countDocuments({type: "resort"});
+        const countVilla = await Hotel.countDocuments({type: "villa"});
+        const countCabin = await Hotel.countDocuments({type: "cabin"});
+        res.status(200).json([
+            {type: "hotel", count: countHotel},
+            {type: "apartments", count: countApartment},
+            {type: "resorts", count: countResort},
+            {type: "villas", count: countVilla},
+            {type: "cabins", count: countCabin}
+        ]);
+    }
+    catch(err){
+        next(err);
+    };
+};
