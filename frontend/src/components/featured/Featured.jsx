96bb3d40aa6b86382ce4018c7a699c2109753995
AworThales <thals4jesus@gmail.com>
2023-08-12 14:34:48 +0100

    Updating file with backend data

--- frontend/src/components/featured/Featured.jsx
+++ frontend/src/components/featured/Featured.jsx
@@ -1,8 +1,16 @@
 import "./featured.css";
+import useFetch from "../../hooks/useFetch";
 
 export default function Featured() {
+
+    const { data, loading, error } = useFetch("hotels/countByCity?cities=Akamkpa,Awi,Calabar");
+    // console.log(data);
+
     return (
      <div className="featured">
+        { loading ?  ("please wait loading...")
+        : (
+        <> 
         <div className="featuredItem">
             <img
                 className="featuredImg"
@@ -11,32 +19,33 @@ export default function Featured() {
             />
             <div className="featuredTitles">
                 <h1>Sulai</h1>
-                <h2>200 properties</h2>
+                <h2>{data[0]} properties</h2>
             </div>
         </div>
-      
-      <div className="featuredItem">
-            <img
-            src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
-            alt=""
-            className="featuredImg"
-            />
-            <div className="featuredTitles">
-            <h1>Blesing</h1>
-            <h2>330 properties</h2>
-            </div>
-      </div>
-      <div className="featuredItem">
-            <img
-            src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
-            alt=""
-            className="featuredImg"
-            />
-            <div className="featuredTitles">
-            <h1>Thales</h1>
-            <h2>400 properties</h2>
-            </div>
-      </div>
+        
+        <div className="featuredItem">
+                <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+                alt=""
+                className="featuredImg"
+                />
+                <div className="featuredTitles">
+                <h1>Blesing</h1>
+                <h2>{data[1]} properties</h2>
+                </div>
+        </div>
+        <div className="featuredItem">
+                <img
+                src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+                alt=""
+                className="featuredImg"
+                />
+                <div className="featuredTitles">
+                <h1>Thales</h1>
+                <h2>{data[2]} properties</h2>
+                </div>
+        </div>
+        </> )}
      </div>
     );
   }
